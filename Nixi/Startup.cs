0723ddec9322bb169d693bf5189598f286b5dad9
Kristof <kristof.volcano@gmail.com>
2018-01-09 22:31:26 +0100

    Add project files.

---
+++ Nixi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nixi.Startup))]
+namespace Nixi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
